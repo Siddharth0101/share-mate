8359d96f5efd36fd1e316ec9f7daca00677d5452
Siddharth-antino <siddsikarwar2001@gmail.com>
2024-11-23 18:52:29 +0530

    update

--- src/components/pages/home/home.js
+++ src/components/pages/home/home.js
@@ -2,7 +2,7 @@ export default function Home() {
     return <div>
      <div className="relative "> 
      <div className="absolute inset-0 flex items-center justify-center  p-4 text-white text-2xl font-bold bg-black/50 z-10">
-        When your friends think 'I'll pay you back' means 'I hope you forget!' 😂
+        When your friends think 'I'll pay you back' means 'I hope you forget!' 😂----NoAbuseGamer--AKA--aayushya
         </div>
         <div>
             <img src="/forgot.png" alt="home " className="w-full h-screen object-cover"/>
