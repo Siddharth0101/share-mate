d59305b622b322d4dfdbd6e0d69df95d3d2c1be8
Siddharth-antino <siddsikarwar2001@gmail.com>
2024-11-24 20:49:26 +0530

    register Modal UI

--- src/components/authentication/registerModal.js
+++ src/components/authentication/registerModal.js
@@ -1,10 +1,10 @@
 import { Form } from "react-router-dom";
 
 export default function RegisterModal() {
-    return <div className="lg:flex hidden  justify-center">
+    return <div className="flex   justify-center">
         <div className="w-[40vw] h-[40vh] bg-white rounded-2xl m-44">
             <Form>
-                <div className="flex justify-center text-xl text-black m-1 ">
+                <div className="flex justify-center text-2xl text-black m-1 ">
                     Register
                 </div>
                 <div>
@@ -12,7 +12,7 @@ export default function RegisterModal() {
                         User Name
                     </div>
                     <div className="m-2">
-                        <input type="text" className="bg-slate-400 rounded-md w-full"/>
+                        <input type="text" className="bg-slate-400 rounded-md w-full text-xl"/>
                     </div>
                 </div>
                 <div>
@@ -20,7 +20,7 @@ export default function RegisterModal() {
                         Email
                     </div>
                     <div className="m-2">
-                        <input type="email" className="bg-slate-400 rounded-md w-full"/>
+                        <input type="email" className="bg-slate-400 rounded-md w-full text-xl"/>
                     </div>
                 </div>
                 <div>
@@ -28,7 +28,7 @@ export default function RegisterModal() {
                         Password
                     </div>
                     <div className="m-2">
-                        <input type="text" className="bg-slate-400 rounded-md w-full"/>
+                        <input type="password" className="bg-slate-400 rounded-md w-full text-xl"/>
                     </div>
                 </div>
                 <div>
@@ -36,11 +36,11 @@ export default function RegisterModal() {
                         Confirm Password
                     </div>
                     <div className="m-2">
-                        <input type="text" className="bg-slate-400 rounded-md w-full"/>
+                        <input type="text" className="bg-slate-400 rounded-md w-full text-xl"/>
                     </div>
                 </div>
                 <div className="flex justify-center">
-                <button className="outline bg-gray-600 hover:bg-green-800 m-7 w-[50vw] rounded-3xl">Register</button>
+                <button className="outline bg-gray-600 hover:bg-green-800 m-1 w-[50vw] rounded-3xl text-white text-2xl p-1">Register</button>
                 </div>
             </Form>
         </div>
